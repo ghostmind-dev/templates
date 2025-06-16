dd5d9fb868c1c53ffcd51c7aa7c5a5cb037d0d4a
Francois Seguin <admin@ghostmind.dev>
2025-06-16 14:06:41 +0000

    moving to dev

--- templates/app-hasura-db/app/state/migrations/default/1700000000000_init/up.sql
+++
@@ -1,6 +0,0 @@
--- Initial migration
--- This migration establishes the baseline for the database
--- No schema changes are made in this initial migration
-
--- This comment serves as a placeholder to ensure the migration system is initialized
-SELECT 1; 
--- templates/app-hasura-db/scripts/test.ts
+++ templates/app-hasura-db/scripts/test.ts
@@ -4,21 +4,23 @@ import { gql, GraphQLClient } from 'npm:graphql-request@7.1.0';
 export default async function (_arg: CustomArgs, opts: CustomOptions) {
   const endpoint_login = Deno.env.get('HASURA_GRAPHQL_ENDPOINT') as string;
 
-  const endpoint_secret = Deno.env.get('HASURA_GRAPHQL_ADMIN_SECRET') as string;
+  const endpoint_secret = Deno.env.get('HASURA_GRAPHQL_ENDPOINT') as string;
 
-  const graphQLClient = new GraphQLClient(`${endpoint_login}/v1/graphql`, {
-    headers: {
-      'X-Hasura-Admin-Secret': endpoint_secret,
-    },
-  });
+  console.log(endpoint_secret);
 
-  const sendRequest = gql`
-    query MyQuery {
-      __typename ## Placeholder value
-    }
-  `;
+  // const graphQLClient = new GraphQLClient(`${endpoint_login}/v1/graphql`, {
+  //   headers: {
+  //     'X-Hasura-Admin-Secret': endpoint_secret,
+  //   },
+  // });
 
-  const response = await graphQLClient.request(sendRequest);
+  // const sendRequest = gql`
+  //   query MyQuery {
+  //     __typename ## Placeholder value
+  //   }
+  // `;
 
-  console.log(response);
+  // const response = await graphQLClient.request(sendRequest);
+
+  // console.log(response);
 }
