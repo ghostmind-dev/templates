ef50b6017b8707eba64e7847cb6f67f6fd73e55b
Francois Seguin <admin@ghostmind.dev>
2025-06-20 01:22:18 +0000

    updating app-next-auth

--- templates/app-next-auth/infra/variables.tf
+++ templates/app-next-auth/infra/variables.tf
@@ -1,20 +1,15 @@
 # variables.tf
 
-variable "ENVIRONMENT" {}
 variable "PORT" {}
 variable "GOOGLE_OAUTH_CLIENT_ID" {}
 variable "GOOGLE_OAUTH_CLIENT_SECRET" {}
+variable "DB_USERS_ENDPOINT" {}
+variable "DB_USERS_SECRET" {}
+variable "HASURA_GRAPHQL_JWT_SECRET" {}
+variable "ENVIRONMENT" {}
 variable "NEXTAUTH_URL" {}
 variable "NEXTAUTH_SECRET" {}
 variable "AUTH_TRUST_HOST" {}
-variable "DB_USERS_ENDPOINT" {}
-variable "DB_USERS_SECRET" {}
-variable "DB_POTION_ENDPOINT" {}
-variable "DB_POTION_SECRET" {}
-variable "NEXT_PUBLIC_DB_POTION_ENDPOINT_WS" {}
-variable "NEXT_PUBLIC_DB_POTION_ENDPOINT_HTTP" {}
-variable "HASURA_GRAPHQL_JWT_SECRET" {}
-variable "CRYPTO_SECRET_KEY" {}
 variable "IMAGE_DIGEST_DEFAULT" {}
 variable "PROJECT" {}
 variable "APP" {}
@@ -24,16 +19,28 @@ variable "GCP_PROJECT_ID" {}
 locals {
   env_vars = [
     {
-      name  = "ENVIRONMENT"
-      value = var.ENVIRONMENT
-    },
-    {
       name  = "GOOGLE_OAUTH_CLIENT_ID"
       value = var.GOOGLE_OAUTH_CLIENT_ID
     },
     {
       name  = "GOOGLE_OAUTH_CLIENT_SECRET"
       value = var.GOOGLE_OAUTH_CLIENT_SECRET
+    },
+    {
+      name  = "DB_USERS_ENDPOINT"
+      value = var.DB_USERS_ENDPOINT
+    },
+    {
+      name  = "DB_USERS_SECRET"
+      value = var.DB_USERS_SECRET
+    },
+    {
+      name  = "HASURA_GRAPHQL_JWT_SECRET"
+      value = var.HASURA_GRAPHQL_JWT_SECRET
+    },
+    {
+      name  = "ENVIRONMENT"
+      value = var.ENVIRONMENT
     },
     {
       name  = "NEXTAUTH_URL"
@@ -46,38 +53,6 @@ locals {
     {
       name  = "AUTH_TRUST_HOST"
       value = var.AUTH_TRUST_HOST
-    },
-    {
-      name  = "DB_USERS_ENDPOINT"
-      value = var.DB_USERS_ENDPOINT
-    },
-    {
-      name  = "DB_USERS_SECRET"
-      value = var.DB_USERS_SECRET
-    },
-    {
-      name  = "DB_POTION_ENDPOINT"
-      value = var.DB_POTION_ENDPOINT
-    },
-    {
-      name  = "DB_POTION_SECRET"
-      value = var.DB_POTION_SECRET
-    },
-    {
-      name  = "NEXT_PUBLIC_DB_POTION_ENDPOINT_WS"
-      value = var.NEXT_PUBLIC_DB_POTION_ENDPOINT_WS
-    },
-    {
-      name  = "NEXT_PUBLIC_DB_POTION_ENDPOINT_HTTP"
-      value = var.NEXT_PUBLIC_DB_POTION_ENDPOINT_HTTP
-    },
-    {
-      name  = "HASURA_GRAPHQL_JWT_SECRET"
-      value = var.HASURA_GRAPHQL_JWT_SECRET
-    },
-    {
-      name  = "CRYPTO_SECRET_KEY"
-      value = var.CRYPTO_SECRET_KEY
     },
     {
       name  = "IMAGE_DIGEST_DEFAULT"
