413094f1ef1128890b04fb9e860398eceddff5de
Francois Seguin <admin@ghostmind.dev>
2025-06-20 01:13:32 +0000

    updating app-next-auth

--- templates/app-next-auth/scripts/test.ts
+++ templates/app-next-auth/scripts/test.ts
@@ -1,25 +1,25 @@
-import type { CustomArgs, CustomOptions } from 'jsr:@ghostmind/run';
-import { fetch } from 'npm:zx@8.1.2';
+import type { CustomArgs, CustomOptions } from "jsr:@ghostmind/run";
+import { fetch } from "npm:zx@8.1.2";
 
 export default async function (_arg: CustomArgs, opts: CustomOptions) {
   const { url, port, has } = opts;
   const { tunnel, local } = url;
 
-  if (has('tunnel')) {
+  if (has("tunnel")) {
     try {
       await fetch(tunnel);
-      console.log('Tunnel is up and running');
+      console.log("Tunnel is up and running");
     } catch (error) {
-      console.error('Tunnel is down');
+      console.error("Tunnel is down");
     }
   }
 
-  if (has('local')) {
+  if (has("local")) {
     try {
       await fetch(`${local}:${port}`);
-      console.log('Local server is up and running');
+      console.log("Local server is up and running");
     } catch (error) {
-      console.error('Local server is down');
+      console.error("Local server is down");
     }
   }
 }
