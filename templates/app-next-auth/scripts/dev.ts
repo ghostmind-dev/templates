da20a620752e7c90cf9193a5de2948a0335e99a2
Francois Seguin <admin@ghostmind.dev>
2025-06-21 02:35:15 +0000

    upgrading misc template

--- templates/app-next-auth/scripts/dev.ts
+++ templates/app-next-auth/scripts/dev.ts
@@ -1,22 +1,30 @@
-import { dockerComposeBuild, dockerComposeUp } from "jsr:@ghostmind/run";
-import type { CustomArgs, CustomOptions } from "jsr:@ghostmind/run";
-import { LogEntry, log, $ } from "npm:zx";
-import process from "node:process";
+import { dockerComposeBuild, dockerComposeUp } from 'jsr:@ghostmind/run';
+import type { CustomArgs, CustomOptions } from 'jsr:@ghostmind/run';
+import { $ } from 'npm:zx';
 
-export default async function (_arg: CustomArgs, opts: CustomOptions) {
-  const { run, start } = opts;
+export default async function (args: CustomArgs, opts: CustomOptions) {
+  const { run, has } = opts;
 
-  await start({
-    build: {
-      command: dockerComposeBuild,
-      priority: 998,
-    },
-    tunnel: `${run} tunnel run`,
-    up: {
-      command: dockerComposeUp,
-      options: {
+  if (has('build')) {
+    await dockerComposeBuild({});
+  }
+
+  const processes = [];
+
+  if (has('tunnel')) {
+    const TUNNEL_NAME = Deno.env.get('TUNNEL_NAME');
+    Deno.env.set('NEXTAUTH_URL', `https://${TUNNEL_NAME}`);
+
+    processes.push($`${run} tunnel run`);
+  }
+
+  if (has('up')) {
+    processes.push(
+      dockerComposeUp({
         forceRecreate: true,
-      },
-    },
-  });
+      })
+    );
+  }
+
+  await Promise.all(processes);
 }
