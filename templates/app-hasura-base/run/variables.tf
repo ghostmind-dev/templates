941c8de8303eeb2b2645131c5028ca791af66557
Francois Seguin <admin@ghostmind.dev>
2025-06-21 22:57:14 +0000

    cleaning

--- templates/app-hasura-base/run/variables.tf
+++ templates/app-hasura-base/run/variables.tf
@@ -1,91 +1,149 @@
 # variables.tf
 
-  variable "PROJECT" {}
-variable "APP" {}
-variable "PORT" {}
-variable "PGHOST" {}
-variable "PGUSER" {}
-variable "PGPASSWORD" {}
-variable "DB_NAME" {}
-variable "HASURA_GRAPHQL_JWT_KEY" {}
-variable "HASURA_GRAPHQL_JWT_SECRET" {}
-variable "HASURA_GRAPHQL_METADATA_DATABASE_URL" {}
-variable "HASURA_GRAPHQL_DATABASE_URL" {}
-variable "HASURA_GRAPHQL_ADMIN_SECRET" {}
-variable "CLOUD_RUN_URL_BASE" {}
-variable "ENVIRONMENT" {}
-variable "HASURA_GRAPHQL_ENDPOINT" {}
-variable "IMAGE_DIGEST_DEFAULT" {}
-variable "GCP_PROJECT_ID" {}
+variable "PROJECT" {
+  type      = string
+  sensitive = true
+}
 
-  
-  locals {
-    env_vars = [
-      {
-        name  = "PROJECT"
-        value = var.PROJECT
-      },
+variable "APP" {
+  type      = string
+  sensitive = true
+}
+
+variable "PORT" {
+  type      = string
+  sensitive = true
+}
+
+variable "PGHOST" {
+  type      = string
+  sensitive = true
+}
+
+variable "PGUSER" {
+  type      = string
+  sensitive = true
+}
+
+variable "PGPASSWORD" {
+  type      = string
+  sensitive = true
+}
+
+variable "DB_NAME" {
+  type      = string
+  sensitive = true
+}
+
+variable "HASURA_GRAPHQL_JWT_SECRET" {
+  type      = string
+  sensitive = true
+}
+
+variable "HASURA_GRAPHQL_METADATA_DATABASE_URL" {
+  type      = string
+  sensitive = true
+}
+
+variable "HASURA_GRAPHQL_DATABASE_URL" {
+  type      = string
+  sensitive = true
+}
+
+variable "HASURA_GRAPHQL_ADMIN_SECRET" {
+  type      = string
+  sensitive = true
+}
+
+variable "CLOUD_RUN_URL_BASE" {
+  type      = string
+  sensitive = true
+}
+
+variable "ENVIRONMENT" {
+  type      = string
+  sensitive = true
+}
+
+variable "HASURA_GRAPHQL_ENDPOINT" {
+  type      = string
+  sensitive = true
+}
+
+variable "IMAGE_DIGEST_DEFAULT" {
+  type      = string
+  sensitive = true
+}
+
+variable "GCP_PROJECT_ID" {
+  type      = string
+  sensitive = true
+}
+
+
+locals {
+  env_vars = [
     {
-        name  = "APP"
-        value = var.APP
-      },
+      name  = "PROJECT"
+      value = var.PROJECT
+    },
     {
-        name  = "PGHOST"
-        value = var.PGHOST
-      },
+      name  = "APP"
+      value = var.APP
+    },
     {
-        name  = "PGUSER"
-        value = var.PGUSER
-      },
+      name  = "PGHOST"
+      value = var.PGHOST
+    },
     {
-        name  = "PGPASSWORD"
-        value = var.PGPASSWORD
-      },
+      name  = "PGUSER"
+      value = var.PGUSER
+    },
     {
-        name  = "DB_NAME"
-        value = var.DB_NAME
-      },
+      name  = "PGPASSWORD"
+      value = var.PGPASSWORD
+    },
     {
-        name  = "HASURA_GRAPHQL_JWT_KEY"
-        value = var.HASURA_GRAPHQL_JWT_KEY
-      },
+      name  = "DB_NAME"
+      value = var.DB_NAME
+    },
     {
-        name  = "HASURA_GRAPHQL_JWT_SECRET"
-        value = var.HASURA_GRAPHQL_JWT_SECRET
-      },
+      name  = "HASURA_GRAPHQL_JWT_SECRET"
+      value = var.HASURA_GRAPHQL_JWT_SECRET
+    },
     {
-        name  = "HASURA_GRAPHQL_METADATA_DATABASE_URL"
-        value = var.HASURA_GRAPHQL_METADATA_DATABASE_URL
-      },
+      name  = "HASURA_GRAPHQL_METADATA_DATABASE_URL"
+      value = var.HASURA_GRAPHQL_METADATA_DATABASE_URL
+    },
     {
-        name  = "HASURA_GRAPHQL_DATABASE_URL"
-        value = var.HASURA_GRAPHQL_DATABASE_URL
-      },
+      name  = "HASURA_GRAPHQL_DATABASE_URL"
+      value = var.HASURA_GRAPHQL_DATABASE_URL
+    },
     {
-        name  = "HASURA_GRAPHQL_ADMIN_SECRET"
-        value = var.HASURA_GRAPHQL_ADMIN_SECRET
-      },
+      name  = "HASURA_GRAPHQL_ADMIN_SECRET"
+      value = var.HASURA_GRAPHQL_ADMIN_SECRET
+    },
     {
-        name  = "CLOUD_RUN_URL_BASE"
-        value = var.CLOUD_RUN_URL_BASE
-      },
+      name  = "CLOUD_RUN_URL_BASE"
+      value = var.CLOUD_RUN_URL_BASE
+    },
     {
-        name  = "ENVIRONMENT"
-        value = var.ENVIRONMENT
-      },
+      name  = "ENVIRONMENT"
+      value = var.ENVIRONMENT
+    },
     {
-        name  = "HASURA_GRAPHQL_ENDPOINT"
-        value = var.HASURA_GRAPHQL_ENDPOINT
-      },
+      name  = "HASURA_GRAPHQL_ENDPOINT"
+      value = var.HASURA_GRAPHQL_ENDPOINT
+    },
     {
-        name  = "IMAGE_DIGEST_DEFAULT"
-        value = var.IMAGE_DIGEST_DEFAULT
-      },
+      name  = "IMAGE_DIGEST_DEFAULT"
+      value = var.IMAGE_DIGEST_DEFAULT
+    },
     {
-        name  = "GCP_PROJECT_ID"
-        value = var.GCP_PROJECT_ID
-      }
-    ]
-  }
-  
+      name  = "GCP_PROJECT_ID"
+      value = var.GCP_PROJECT_ID
+    }
+  ]
+}
+
   
